bedded382fbd26fae6ef20d7278d155e5ed5aa6f
Vladimir <35075229+vladimirmaljm@users.noreply.github.com>
2018-03-14 22:44:33 +0100

    gradebook fix errors

--- gradebook/src/gradebook/Main.java
+++ gradebook/src/gradebook/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		String students[];
+		String studentsName[];
+		String studentsSurname[];
 		int grades[];
 		int totalGrade = 0;
 		int numberOfStudents = 0;
@@ -12,23 +13,27 @@ public class Main {
 		
 				System.out.println("welcome Mrs James please enter the number of students in your class: ");
 				numberOfStudents = sc.nextInt();
-				students = new String[numberOfStudents];
+				studentsName = new String[numberOfStudents];
+				studentsSurname = new String[numberOfStudents];
 				grades = new int[numberOfStudents];
 				
 				for(int i = 0 ; i < numberOfStudents; i++) { 
 			
 					int k = i + 1; //zato sto brojac "i" pocinje od nule a brojac "k" treba da oznacava broj studenata kojih ima vise od nula
 			
-					System.out.println("Enter the name and surname of your " + k + ". student: ");
-					students[i] = sc.next();
+					System.out.println("Enter the name of your " + k + ". student: ");
+					studentsName[i] = sc.next();
+					
+					System.out.println("Enter the surname of your " + k + ". student: ");
+					studentsSurname[i] = sc.next();
 			
-					System.out.println("Enter the grade: ");
+					System.out.println("Enter his grade: ");
 					grades[i]= sc.nextInt();
 				}
 				
 				for(int i = 0 ; i < numberOfStudents; i++) { 
 					
-					System.out.println("Student " + students[i] + " " + " has grade " + grades[i]);
+					System.out.println("Student " + studentsName[i] + " " + studentsSurname[i] + " "  + " has grade " + grades[i]);
 					totalGrade += grades[i];
 				}
 				double averageGrade = totalGrade / numberOfStudents;

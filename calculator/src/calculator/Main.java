7317ec7218b89a6f2fe9af6ef784ef50684be65b
Vladimir <35075229+vladimirmaljm@users.noreply.github.com>
2018-05-10 22:28:30 +0200

    update

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -5,26 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int result = 0;
-		try(Scanner sc = new Scanner(System.in)) {
-					
+		try (Scanner scenner = new Scanner(System.in)) {
+				
 			System.out.println("Enter your first number");
-			int firstNumber = sc.nextInt();
+			int firstNumber = scenner.nextInt();
+			
+			
 			
 			System.out.println("Choose your operation +, -, * ,/ ");
-			String operation = sc.next();
+			String operation = scenner.next();
+			
 			if(operation !=  "+" || operation !=  "-" || operation !=  "*" || operation !=  "/") {
 				System.out.println("You enter wrong operation, please enter it again ");
-				String operation1 = sc.next();
-				operation = operation1;
+				operation = scenner.next();
 			}
 				
 			System.out.println("Enter your second number");
-			int secondNumber = sc.nextInt();
-					
+			int secondNumber = scenner.nextInt();
+			
 			if(operation.equals("/") && secondNumber == 0) {				
 				System.out.println("Divide with 0 (zero) can't be calculated.");
 				System.out.println("Enter your second number again: ");
-				secondNumber = sc.nextInt();
+				secondNumber = scenner.nextInt();
 			} else if(operation.equals("+")) {
 				result = firstNumber + secondNumber;
 			} else if(operation.equals("-")) {
